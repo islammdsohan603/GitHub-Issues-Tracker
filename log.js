75fdad01756b3cee58f753828888d3ae6a900d19
islammdsohan603 <islammdsohan603@gmail.com>
2026-03-08 10:21:51 +0600

    first commit

---
+++ log.js
@@ -0,0 +1,13 @@
+const userInput = document.getElementById("username-input");
+const passwordInput = document.getElementById("password-input");
+const signInButton = document.getElementById("sign-in-button");
+
+signInButton.addEventListener("click", () => {
+  const username = userInput.value;
+  const password = passwordInput.value;
+  if (username === "admin" && password === "admin123") {
+    window.location.href = "index.html";
+  } else {
+    alert("Invalid username or password");
+  }
+});
